5b9e99b7b991f9559159bdb947c8d1c5560305e9
chibielora <chibi.elora@gmail.com>
2020-08-12 10:08:15 -0400

    thunkmodel

--- src/store.js
+++ src/store.js
@@ -8,3 +8,24 @@ const store = createStore(
 )
 
 export default store
+
+// function createThunkMiddleware(extraArgument) {
+//   return ({
+//     dispatch,
+//     getState
+//   }) => next => action => {
+//     // This gets called for every action you dispatch.
+//     // If it's a function, call it.
+//     if (typeof action === 'function') {
+//       return action(dispatch, getState, extraArgument);
+//     }
+
+//     // Otherwise, just continue processing this action as usual
+//     return next(action);
+//   };
+// }
+
+// const thunk = createThunkMiddleware();
+// thunk.withExtraArgument = createThunkMiddleware;
+
+// export default thunk;
